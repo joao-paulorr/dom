2c234633a07f8c1782221c343379e7465c32663d
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-05-29 18:15:40 +0000

    a

---
+++ src/script.js
@@ -0,0 +1,4 @@
+//quando escrevemos window.document, temos varias opções.
+// var titulo = window.document.write ("Qualquer coisa")
+// window.document.write ("<h1>Olá Mundo</h1>")
+window.document.getElementsByTagName ("p")[0]
